4a01b9793191cd656584848204b040e8447cf920
pedro.rodrigues1 <pedro.rodrigues@semantix.com.br>
2020-05-15 20:44:38 -0300

    add screen produtos

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -14,14 +14,14 @@ def load_user(id):
     return User.query.filter_by(id=id).first()
 
 
-@app.route("/index/") # Decorator onde é passado a rota
-@app.route("/")
+@app.route("/home/") # Decorator onde é passado a rota
+@app.route("/home")
 def index():
     return render_template('index.html')
 
 
-@app.route("/login", methods=["GET", "POST"])
-@app.route("/login/", methods=["GET", "POST"])
+@app.route("/index/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -41,7 +41,11 @@ def login():
 def logout():
     logout_user()
     flash("Logged out.")
-    return redirect(url_for("index"))
+    return redirect(url_for("login"))
+
+@app.route("/produtos")
+def produtos():
+    return render_template('produtos.html')
 
 # @app.route("/teste/<info>")
 # @app.route("/teste", defaults={"info":None})
